9c68767affc8343fd948997c0daa9d76271966c0
Ed Baunton <edbaunton@gmail.com>
2018-06-05 15:51:36 +0100

    Execute the plz output

--- buildall.sh
+++ buildall.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 set -e
 make clean && make && ./hello
+
 rm -rf bld && cmake . -Bbld && ( cd bld && make && ./hello )
+
 rm -rf bazel-* buck-out
+
 buck clean && buck build //... && buck-out/gen/hello
+
 bazel clean && bazel build //... && bazel-bin/hello
-plz build //.:hello
+
+plz build //.:hello && ./plz-out/bin/hello
 
 CXX=cc ./pants compile .:hello && CXX=cc ./pants run .:hello
